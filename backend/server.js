88733560cb27ca0e6ed9c62e10161388bfb3b78f
joselinares17 <josedaniellinares135@gmail.com>
2024-10-06 14:28:54 -0500

    reformating route filenames

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 import connectMongoDb from './db/connectMongoDb.js';
 
 
@@ -24,8 +25,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
